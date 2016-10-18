11cb00772f20fc986bbd77ae20d16ebfc754781f
Rogerio Lucio <r_322@live.com>
2016-10-17 22:49:58 -0200

    inter

---
+++ responsivo/index.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="css.css">
+  <title></title>
+</head>
+<body>
+  <div class="container">
+      <div class="home">
+      <div class="box-texto1">
+          <h1>Surprise Yourself<br>
+With the Quality of Brazil</h1>
+      </div>
+
+      <div class="conteudo2">
+      <h2>
+              HALL 17, STAND 17A38 – NOVEMBER 16TH TO 19TH 2015 – DÜSSELDORF, GERMANY
+          </h2>
+          </a><h4><a href="/medica/companies.php">
+          </a><a href="/medica/companies.php">Click here to check all Brazilian companies at MEDICA <br> and Schedule a meeting with them during the show</a>
+          </h4>
+      </div>
+      </div>
+
+      </header>
+
+  </div>
+
+</body>
+</html>
